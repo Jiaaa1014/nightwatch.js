57690e07167770291764500d17a3fd7da1548b18
Jiaaa1014 <linao264590000@gmail.com>
2018-03-07 15:37:30 +0800

    arrange the test files

--- tests/artsyjewels/case2.js
+++ tests/artsyjewels/case2.js
@@ -7,8 +7,11 @@ module.exports = {
       .url(`https://www.artsyjewels.com/`)
       .waitForElementVisible('body', 1000)
       .assert.title('Artsyjewels- High quality fashion jewelry at a reasonable price')
-      .setValue('#shopify-section-header > div > header > div > div.grid__item.small--hide.medium-up--one-third > div > form > input', ['pearl necklace', browser.Keys.ENTER])
-      .pause(5000)
+      .setValue('#shopify-section-header > div > header > div > div.grid__item.small--hide.medium-up--one-third > div > form > input', ['silver bracelets', browser.Keys.ENTER])
+      .pause(1000)
+    browser
+      .assert.title('silver bracelets – Artsyjewels')
+      .assert.urlContains('https://www.artsyjewels.com/search?q=silver+bracelets');
     browser.saveScreenshot('./screenshots/case2-2.jpg')
       .end()
   }
---
+++ tests/artsyjewels/case4.js
@@ -0,0 +1,16 @@
+// 忘記密碼
+
+module.exports = {
+  tags: ['testcase4'],
+  'Go to Login Page and existign user should login': function (browser) {
+    browser
+      .url('http://www.artsyjewels.com')
+      .waitForElementVisible('body', 1000)
+      .click('#shopify-section-header > div > header > div > div.grid__item.small--one-half.medium-up--one-third.text-right.site-header__icons.site-header__icons--plus > div > a.site-header__account')
+      .click('#RecoverPassword')
+      .setValue('#RecoverEmail', ['linao2645900@yahoo.com.tw', browser.Keys.ENTER])
+      // .click('#RecoverPasswordForm > div.form-vertical > form > div > p > input')
+      .pause(3000)
+      .end();
+  }
+};
